0447a3d89e54d890a1bc231f9f4fd3d38633ac09
ssarl <ssarl1180@gmail.com>
2020-03-18 15:20:38 +0200

    Automate eslint --fix

--- npmi.sh
+++ npmi.sh
@@ -3,5 +3,10 @@
 cd backend
 npm install
 
+node_modules/.bin/eslint src/index.js --fix
+
 cd ../frontend
 npm install
+
+node_modules/.bin/eslint src/index.jsx webpack.config.js --fix
+sed -i 's/0.0.0.0:9000/3.126.69.19:9000/' webpack.config.js
